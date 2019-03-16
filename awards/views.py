2260ffd1213a04c53d44cc12dfb728a062237184
dunyung1 <dshege4@gmail.com>
2019-03-16 20:57:14 +0300

    add views for register

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(request):
     form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
--- pongezi/settings.py
+++ pongezi/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'awards.app.AwardsConfig',
+    'awards.apps.AwardsConfig',
     'bootstrap4'
 ]
 

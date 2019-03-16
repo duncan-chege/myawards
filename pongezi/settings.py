8e335937f98d9b8a4c9b2dcc8039a599acf36797
dunyung1 <dshege4@gmail.com>
2019-03-16 19:49:44 +0300

    install bootstrap4

--- pongezi/settings.py
+++ pongezi/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'awards.app.AwardsConfig'
+    'awards.app.AwardsConfig',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [

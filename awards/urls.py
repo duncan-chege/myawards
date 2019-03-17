3467834cd4c64fa9254b08e4ba2acb6083c834b7
dunyung1 <dshege4@gmail.com>
2019-03-17 14:38:24 +0300

    add Project Model to admin page

--- awards/urls.py
+++ awards/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url(r'^register',views.register, name='register'),
     url(r'^profile',views.profile, name='profile') 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

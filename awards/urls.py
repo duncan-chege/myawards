52bb76523de38462c0b12dceb8bcbe7b66d340d5
dunyung1 <dshege4@gmail.com>
2019-03-17 07:45:35 +0300

    create a login view and css file

--- awards/urls.py
+++ awards/urls.py
@@ -5,5 +5,4 @@ from django.contrib.auth import views
 urlpatterns=[
     url('^$',views.register, name='register'),
     url(r'^login',views.LoginView, name='login'),
-    url(r'^logout',views.LogoutView, name='logout')
 ]
--- awards/views.py
+++ awards/views.py
@@ -13,3 +13,6 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def login(request):
+    return render(request, 'users/login.html')

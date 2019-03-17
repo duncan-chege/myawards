b3650a475b1b5b6cd56f10b19942ddb5a96587a6
dunyung1 <dshege4@gmail.com>
2019-03-17 07:25:22 +0300

    create login and logout urls

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views
 
 urlpatterns=[
     url('^$',views.register, name='register'),
+    url(r'^login',views.LoginView, name='login'),
+    url(r'^logout',views.LogoutView, name='logout')
 ]
--- pongezi/urls.py
+++ pongezi/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+    url('', include('awards.urls')),
     url(r'^admin/', admin.site.urls),
-    url('', include('awards.urls'))
 ]

63808f77ac50f67801048ee0e612f15d48703900
dunyung1 <dshege4@gmail.com>
2019-03-17 14:27:19 +0300

    create project class and media url in settings py file

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length =50)
+    description = models.CharField(max_length =100)
+    image_path = models.ImageField(upload_to='project_images/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    website_link= models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.title
+
--- pongezi/settings.py
+++ pongezi/settings.py
@@ -130,3 +130,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'login' 
 LOGIN_URL='login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
